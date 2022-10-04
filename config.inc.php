4245bee4cca9abb6351bd34f7fa464c4e62d23b4
Jsavy <jsave586@mtroyal.ca>
2022-10-04 15:01:56 -0600

    updated config file and tree structure for v1.2

--- config.inc.php
+++ config.inc.php
@@ -3,6 +3,6 @@ define('DBHOST', 'localhost');
 define('DBNAME', 'music');
 define('DBUSER', 'root');
 define('DBPASS', '');
-//define('DBCONNSTRING','sqlite:./music.db');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+define('DBCONNSTRING','sqlite:./data/music.db');
+//define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
