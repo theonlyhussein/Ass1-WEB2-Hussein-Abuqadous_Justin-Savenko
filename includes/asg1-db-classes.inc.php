f3309fa44c5f5ab9b5589d454d13901cacd8c1d6
Jsavy <jsave586@mtroyal.ca>
2022-10-08 19:43:12 -0600

    Added db class, added definition in config, begin singleSongPage

---
+++ includes/asg1-db-classes.inc.php
@@ -0,0 +1,54 @@
+<?php 
+class DatabaseHelper { 
+ /* Returns a connection object to a database */
+ public static function createConnection( $values=array() ) { 
+ $connString = $values[0]; 
+ $user = $values[1]; 
+ $password = $values[2]; 
+ $pdo = new PDO($connString,$user,$password); 
+ $pdo->setAttribute(PDO::ATTR_ERRMODE, 
+ PDO::ERRMODE_EXCEPTION); 
+ $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, 
+ PDO::FETCH_ASSOC); 
+ return $pdo; 
+ } 
+ /*
+ Runs the specified SQL query using the passed connection and 
+ the passed array of parameters (null if none)
+ */
+ public static function runQuery($connection, $sql, $parameters) { 
+ $statement = null; 
+ // if there are parameters then do a prepared statement
+ if (isset($parameters)) { 
+ // Ensure parameters are in an array
+ if (!is_array($parameters)) { 
+ $parameters = array($parameters); 
+ } 
+ // Use a prepared statement if parameters 
+ $statement = $connection->prepare($sql); 
+ $executedOk = $statement->execute($parameters); 
+ if (! $executedOk) throw new PDOException; 
+ } else { 
+ // Execute a normal query 
+ $statement = $connection->query($sql); 
+ if (!$statement) throw new PDOException; 
+ } 
+ return $statement; 
+ } 
+}
+class SingleSongViewDB {
+    private static $baseSQL = "SELECT song_id, title, artists.artist_name, types.type_name, genres.genre_name, year, duration, bpm, energy, danceability, liveness, valence, acousticness, speechiness, popularity
+                                FROM songs
+                                INNER JOIN artists ON artists.artist_id = songs.artist_id
+                                INNER JOIN types ON types.type_id = artists.artist_type_id
+                                INNER JOIN genres ON genres.genre_id = songs.genre_id ";
+    public function __construct($connection) {
+        $this->pdo = $connection;
+    }
+
+    public function getAll() {
+        $sql = self::$baseSQL;
+        $statement = DatabaseHelper::runQuery($this->pdo, $sql, null);
+        return $statement->fetchAll();
+    }
+}
---
+++ singleSongPage.php
@@ -0,0 +1,39 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+require_once 'includes/asg1-db-classes.inc.php';
+require_once 'includes/config.inc.php';
+
+try {
+    $conn = DatabaseHelper::createConnection(array(DBCONNSTRING, DBUSER, DBPASS));
+    $songsGateway = new SingleSongViewDB($conn);
+    $songs = $songsGateway->getAll();
+    $song_id = defaultSONG_id;
+    if(isset($_GET['song_id']) && $_GET['song_id'] > 1000 && $_GET['song_id'] < 1319)
+        $song_id = $_GET['song_id'];
+}catch (Exception $e) { die($e->getMessage());}
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <title>COMP 3512 Assign1</title>
+        <p class="subtitle">Hussein Abuqadous & Justin Savenko</p>
+    </head>
+<body>
+    <main>
+        <?php echo $song_id;?>
+    </main>
+<footer class="ui black inverted segment">
+  <div class="ui container">COMP 3512 Fall 2022</div>
+  <div class="ui container">Hussein Abuqadous & Justin Savenko &#169</div>
+  <div class="ui container"><a href ="https://github.com/theonlyhussein/Ass1-WEB2-Hussein-Abuqadous_Justin-Savenko">Github Repo</a></div>
+  <div class="ui container"><a href ="https://github.com/theonlyhussein">Hussein Github</a></div>
+  <div class="ui container"><a href ="https://github.com/Jsavy">Justin Github</a></div>
+</footer>
+</body>
+</html>
