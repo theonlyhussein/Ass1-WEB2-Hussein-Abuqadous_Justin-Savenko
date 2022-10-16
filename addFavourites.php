14c6ec683d3b84e843830ba4b4e94f3bfe727e6b
Jsavy <jsave586@mtroyal.ca>
2022-10-16 04:22:48 -0600

    addition logic

--- addFavourites.php
+++ addFavourites.php
@@ -1,5 +1,15 @@
 <?php
 session_start();
 
+if(isset($_GET['song_id']) && ! empty($_GET['song_id'])) {
+
+    if(! isset($_SESSION['favourites'])) {
+        $favourites = array();
+        $_SESSION['favourites'] = $favourites;
+    }
+    $favourites = $_SESSION['favourites'];
+    $favourites[] = array($_GET['song_id'],$_GET['title'],$_GET['artist'],$_GET['year'],$_GET['genre'],$_GET['popularity']);
+    $_SESSION['favourites'] = $favourites;
+}
 header("Location: viewFavourites.php");
 ?>
