e23c72de88d200ac526525ecb57e4710cb5f86b6
Jsavy <jsave586@mtroyal.ca>
2022-10-16 05:08:39 -0600

    structure change

---
+++ includes/progressBar.inc.php
@@ -0,0 +1,128 @@
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #00ff43; --num:<?php echo $row['bpm']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['bpm']?><span>bpm</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">bpm</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #ed6c02; --num:<?php echo $row['energy']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['energy']?><span>%</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">Energy</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #0f6fbd; --num:<?php echo $row['danceability']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['danceability']?><span>%</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">Danceability</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #9805b5; --num:<?php echo $row['liveness']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['liveness']?><span>%</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">Liveness</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #1d05f5; --num:<?php echo $row['valence']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['valence']?><span>%</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">Valence</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #05f59d; --num:<?php echo $row['acousticness']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['acousticness']?><span>%</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">Acousticness</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #9fc40a; --num:<?php echo $row['speechiness']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['speechiness']?><span>%</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">Speechiness</h2>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="card">
+        <div class="box">
+            <div class="percent" style="--clr: #f7051d; --num:<?php echo $row['popularity']?>;">
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <svg>
+                        <div class="num">
+                            <h2><?php echo $row['popularity']?><span>%</span></h2>
+                        </div>
+            </div>
+            <h2 class="text">Popularity</h2>
+        </div>
+    </div>
+</div>
